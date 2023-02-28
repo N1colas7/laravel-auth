f1284207ea4c0103588eabb1a45bb89bc0e948fd
N1colas7 <nicolasnd9897@gmail.com>
2023-02-28 17:37:58 +0100

    modify route in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\DashboardController as DashboardController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,9 +21,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//MODIFICHIAMO QUESTA ROTTA PERCHE' ABBIAMO UN CONTROLLER CHE CI RESTITUISCE LA VIEW
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard',[DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
